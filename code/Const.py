9f75edb2ea12db253a807eaa2c8f4600850892fa
wal_w <waldennyr@gmail.com>
2026-03-14 18:47:16 -0300

    Creating player Shot Creating enemy Shot

--- code/Const.py
+++ code/Const.py
@@ -16,9 +16,14 @@ ENTITY_SPEED = {
     'Level1Bg5':5,
     'Level1Bg6':6,
     'Player1' : 3,
+    'Player1Shot': 1,
     'Player2' : 3,
-    'Enemy1' : 2,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
     'Enemy2' : 1,
+    'Enemy2Shot': 2,
+
 }
 
 ENTITY_HEALTH = {
@@ -35,15 +40,21 @@ ENTITY_HEALTH = {
     'Level2Bg3' : 999,
     'Level2Bg4' : 999,
     'Player1' : 300,
-    'Player1Shoot' : 1,
+    'Player1Shot' : 1,
     'Player2' : 300,
     'Player2Shot' : 1,
     'Enemy1' : 50,
-    'Enemy1Shoot' : 1,
+    'Enemy1Shot' : 1,
     'Enemy2' : 60,
     'Enemy2Shot' : 1,
 }
 
+ENTITY_SHOT_DELAY = {
+    'Player1' : 20,
+    'Player2' : 15,
+    'Enemy1' : 100,
+    'Enemy2' : 200,
+}
 EVENT_ENEMY = pygame.USEREVENT + 1
 
 #M
@@ -61,7 +72,7 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
                  'Player2' :pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                  'Player2' :pygame.K_d}
-PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_SPACE,
                  'Player2' :pygame.K_LCTRL}
 
 #S
--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,5 +1,8 @@
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class EntityMediator:
@@ -7,11 +10,14 @@ class EntityMediator:
     @staticmethod
     def __verify_colision_window(ent: Entity):
         if isinstance(ent, Enemy):
-            if ent.rect.right < 0:
+            if ent.rect.right <= 0:
                 ent.health = 0
-
-
-
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >=WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
+            if ent.rect.right <= 0:
+                ent.health = 0
 
             pass
 
