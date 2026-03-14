d2c25d2a61bcbd4bd2d4f7a1c3b00b330fe53326
wal_w <waldennyr@gmail.com>
2026-03-14 17:27:57 -0300

    Creating Mediator Pattern. Enemies destruction.

--- code/Const.py
+++ code/Const.py
@@ -21,6 +21,29 @@ ENTITY_SPEED = {
     'Enemy2' : 1,
 }
 
+ENTITY_HEALTH = {
+    'Level1Bg0' : 999,
+    'Level1Bg1' : 999,
+    'Level1Bg2' : 999,
+    'Level1Bg3' : 999,
+    'Level1Bg4' : 999,
+    'Level1Bg5' : 999,
+    'Level1Bg6' : 999,
+    'Level2Bg0' : 999,
+    'Level2Bg1' : 999,
+    'Level2Bg2' : 999,
+    'Level2Bg3' : 999,
+    'Level2Bg4' : 999,
+    'Player1' : 300,
+    'Player1Shoot' : 1,
+    'Player2' : 300,
+    'Player2Shot' : 1,
+    'Enemy1' : 50,
+    'Enemy1Shoot' : 1,
+    'Enemy2' : 60,
+    'Enemy2Shot' : 1,
+}
+
 EVENT_ENEMY = pygame.USEREVENT + 1
 
 #M
---
+++ code/EntityMediator.py
@@ -0,0 +1,28 @@
+from code.Enemy import Enemy
+from code.Entity import Entity
+
+
+class EntityMediator:
+
+    @staticmethod
+    def __verify_colision_window(ent: Entity):
+        if isinstance(ent, Enemy):
+            if ent.rect.right < 0:
+                ent.health = 0
+
+
+
+
+            pass
+
+    @staticmethod
+    def verify_colision(entity_list: list [Entity]):
+        for i in range(len(entity_list)):
+            test_entity = entity_list[i]
+            EntityMediator.__verify_colision_window(test_entity)
+
+    @staticmethod
+    def verify_health(entity_list: list[Entity]):
+        for ent in entity_list:
+            if ent.health <=0:
+                entity_list.remove(ent)
