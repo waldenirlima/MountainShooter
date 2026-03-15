cc59933e91f0a7138f24b399aca0e8eddaf8d446
wal_w <waldennyr@gmail.com>
2026-03-15 00:59:33 -0300

    SqLite3 implementation for Score using proxy design pattern

--- code/Score.py
+++ code/Score.py
@@ -1,9 +1,14 @@
+import sys
+from datetime import datetime
+
 import pygame
+from pygame.constants import KEYDOWN, K_RETURN, K_BACKSPACE, K_ESCAPE
 from pygame.font import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.Const import COLOR_YELLOW, SCORE_POS, MENU_OPTION
+from code.Const import COLOR_YELLOW, SCORE_POS, MENU_OPTION, COLOR_WHITE
+from code.DBProxy import DBProxy
 
 
 class Score:
@@ -17,13 +22,40 @@ class Score:
     def save(self, game_mode: str, player_score: list[int]):
         pygame.mixer_music.load('./asset/Score.mp3')
         pygame.mixer_music.play(-1)
-        self.window.blit(source=self.surf, dest=self.rect)
+        db_proxy = DBProxy('DBScore')
+        name = ''
         while True:
+            self.window.blit(source=self.surf, dest=self.rect)
             self.score_text(48, 'YOU WIN!!', COLOR_YELLOW, SCORE_POS['Title'])
             if game_mode == MENU_OPTION[0]:
-                text = 'Player1 enter your name (4 caracters):'
+                score = player_score[0]
+                text = 'Enter Player1 name (4 caracters):'
+            if game_mode == MENU_OPTION[1]:
+                score = (player_score[0] + player_score[1]) / 2
+                text = 'Enter Team name (4 caracters):'
+            if game_mode == MENU_OPTION[2]:
+                if player_score[0] >= player_score[1]:
+                    score = player_score[0]
+                    text = 'Enter Player1 name (4 caracters):'
+                else:
+                    score = player_score[1]
+                    text = 'Enter Player2 name (4 caracters):'
+            self.score_text(20, text, COLOR_WHITE, SCORE_POS['EnterName'])
 
-
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_RETURN and len(name) == 4:
+                        db_proxy.save({'name': name, 'score': score, 'date': get_formatted_date()})
+                        self.show()
+                    elif event.key == K_BACKSPACE:
+                        name = name[:-1]
+                    else:
+                        if len(name) < 4:
+                            name += event.unicode
+            self.score_text(20, name, COLOR_WHITE, SCORE_POS['Name'])
             pygame.display.flip()
             pass
 
@@ -31,12 +63,36 @@ class Score:
         pygame.mixer_music.load('./asset/Score.mp3')
         pygame.mixer_music.play(-1)
         self.window.blit(source=self.surf, dest=self.rect)
+        self.score_text(48, 'TOP 10 SCORE', COLOR_YELLOW, SCORE_POS['Title'])
+        self.score_text(20, ' NAME     SCORE              DATE      ', COLOR_YELLOW, SCORE_POS['Label'])
+        db_proxy = DBProxy('DBScore')
+        list_score = db_proxy.retrieve_top10()
+        db_proxy.close()
+
+        for player_score in list_score:
+            id, name, score, date =player_score
+            self.score_text(20, f'{name}     {score :05d}         {date}', COLOR_YELLOW,
+                            SCORE_POS[list_score.index(player_score)])
         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        return
             pygame.display.flip()
-            pass
+
 
     def score_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
+
+
+def get_formatted_date():
+    current_datetime = datetime.now()
+    current_time = current_datetime.strftime("%H:%M")
+    current_date = current_datetime.strftime("%d%m%y")
+    return f"{current_time} - {current_date}"
