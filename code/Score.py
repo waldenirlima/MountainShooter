f3c0f959927478f5b43231cec933ded619c5827b
wal_w <waldennyr@gmail.com>
2026-03-15 01:57:40 -0300

    Final

--- code/Score.py
+++ code/Score.py
@@ -27,9 +27,10 @@ class Score:
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.score_text(48, 'YOU WIN!!', COLOR_YELLOW, SCORE_POS['Title'])
+            text = 'Enter Player1 name (4 caracters):'
+            score = player_score[0]
             if game_mode == MENU_OPTION[0]:
                 score = player_score[0]
-                text = 'Enter Player1 name (4 caracters):'
             if game_mode == MENU_OPTION[1]:
                 score = (player_score[0] + player_score[1]) / 2
                 text = 'Enter Team name (4 caracters):'
@@ -50,6 +51,7 @@ class Score:
                     if event.key == K_RETURN and len(name) == 4:
                         db_proxy.save({'name': name, 'score': score, 'date': get_formatted_date()})
                         self.show()
+                        return
                     elif event.key == K_BACKSPACE:
                         name = name[:-1]
                     else:
@@ -70,7 +72,7 @@ class Score:
         db_proxy.close()
 
         for player_score in list_score:
-            id, name, score, date =player_score
+            id, name, score, date = player_score
             self.score_text(20, f'{name}     {score :05d}         {date}', COLOR_YELLOW,
                             SCORE_POS[list_score.index(player_score)])
         while True:
@@ -82,7 +84,6 @@ class Score:
                     if event.key == K_ESCAPE:
                         return
             pygame.display.flip()
-
 
     def score_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
